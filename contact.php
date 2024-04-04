5fd066181b04cc8433ed8bc24f50ccc55a11f0e7
uknowwho <florianvandersteen@gmail.com>
2024-04-04 16:11:25 +0200

    03B: opdr 1.3, issue 5; rode error kleur toegevoegd en basis van error handling

--- contact.php
+++ contact.php
@@ -22,56 +22,84 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $valid = True;
 
     if ($aanhef == "--") {
-        global $gender_error;
-        $gender_error = "Vul alsjeblieft je aanhefvoorkeur in of geef aan dat je dit liever niet laat weten.";
+        $GLOBALS["gender_error"] = "Vul alsjeblieft je aanhefvoorkeur in of geef aan dat je dit liever niet laat weten.";
         $valid = False;
     }
 
     if (empty($name)) {
-        global $name_error;
-        $name_error = "Vul alsjeblieft je volledige naam in.";
+        $GLOBALS["name_error"] = "Vul alsjeblieft je volledige naam in.";
         $valid = False;
     }
 
-    else if (strlen(ctype_alpha($name)) < 1) {
-        global $name_error;  
-        $name_error = "Vul alsjeblieft een naam in met minstens 1 letter.";
+    else if (strlen(ctype_alpha($name)) < 1) {  
+        $GLOBALS["name_error"] = "Vul alsjeblieft een naam in met minstens 1 letter.";
         $valid = False;
 
     if (empty($bericht)) {
-        global $bericht_error;
-        $bericht_error = "Vul alsjeblieft een bericht in.";
+        $GLOBALS["bericht_error"] = "Vul alsjeblieft een bericht in.";
         $valid = False;
     }
 
+    function handle_email($email) {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return [False, "Vul alsjeblieft een geldig emailadres in."];
+        }
+
+        return [True, ""];
+    }
+
+    function handle_phone($phone) {
+        if(!ctype_digit($phone)) {
+            return [False, "Vul alsjeblieft een telefoonnummer in met alleen cijfers."];
+        }
+
+        return [True, ""]; 
+    }
+
+    function handle_post($street, $housingnumber, $postalcode, $municip) {
+        if (empty($street)) {
+            return [False, "Vul alsjeblieft je straatnaam in."];
+        }
+        if (empty($housingnumber)) {
+            return [False, "Vul alsjeblieft je huisnummer in."];
+        }
+
+        if (empty($postalcode)) {
+            return [False, "Vul alsjeblieft je postcode in."];
+        }
+
+        if (empty($municip)) {
+            return [False, "Vul alsjeblieft je gemeente in."];
+        }
+
+        return [True, ""];
+    }
+
+
     switch ($comm) {
         case empty($comm):
-            global $comm_error;
-            $comm_error = "Vul alsjeblieft je communicatievoorkeur in.";
+            $GLOBALS["comm_error"] = "Vul alsjeblieft je communicatievoorkeur in.";
             $valid = False;
             break;
         
         case $comm == "Email":
-            global $comm_error;
-            list($valid, $comm_error) = handle_email();
+            list($valid, $GLOBALS["comm_error"]) = handle_email($email);
             break;
 
         case $comm == "Telefoon":
             global $comm_error;
-            list($valid, $comm_error) = handle_phone();
+            list($valid, $GLOBALS["comm_error"]) = handle_phone($phone);
             break;
     
         case $comm == "Post":
             global $comm_error;
-            list($valid, $comm_error) = handle_post();
+            list($valid, $GLOBALS["comm_error"]) = handle_post($street, $housingnumber, $postalcode, $municip);
             break;
+        }
     }
-}
 
-}
+} ?>
 
-    // moet ik afhandelen als er een get request wordt gestuurd?
-?>
 <body> 
     <h1>Formulierensite</h1>
     <ul class="navbar">
@@ -81,34 +109,56 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </ul>
 
     <h2>Het Contactformulier</h2>
-    <form method="POST" action="contact.php">
-        <p>Neem contact op:</p>
-        <label for="gender">Aanhef: </label>
-        <select id="gender" name="contact">
-            <option value="none">--</option>
-            <option value="woman">Mevr.</option>
-            <option value="man">Dhr.</option>
-            <option value="nonbinary">Non-binair.</option>
-            <option value="decline">Zeg ik liever niet.</option>
-        </select>
-        <label for="fullname"><br>Voor- en achternaam: </label><input type="text" id="fullname" placeholder="Marie Jansen">
-        <label for="email"><br>Mailadres: </label><input type="email" id="email" placeholder="voorbeeld@mail.com">
-        <label for="phonenumber"><br>Telefoonnummer: </label><input type="tel" id="phonenumber" placeholder="0612345678">
-        <label for="street"><br>Straat: </label><input type="text" id="street" placeholder="Voorbeeldstraat">
-        <label for="housingnumber"><br>Huisnummer: </label><input type="number" id="housingnumber" placeholder="1">
-        <label for ="addition"><br>Toevoeging: </label><input type="text" id="addition" placeholder="A"> 
-        <label for="postalcode"><br>Postcode: </label><input type="text" id="postalcode" placeholder="1234AB">
-        <label for="municipality"><br>Gemeente: </label><input type="text" id="municipality" placeholder="Utrecht">
+    <?php if (!$valid) { /* Show the next part only when $valid is false */ ?>
+        <form method="POST" action="contact.php">
+            <p>Neem contact op:</p>
+            <label for="gender">Aanhef: </label>
+            <select id="gender" name="contact" value="<?php echo $gender; ?>">
+                <option value="none">--</option>
+                <option value="woman">Mevr.</option>
+                <option value="man">Dhr.</option>
+                <option value="nonbinary-man">Dhr. / Mevr.</option>
+                <option value="nonbinary-man">Mevr. / Dhr.</option>
+                <option value="decline">Zeg ik liever niet.</option>
+            </select>
+            <span class="error">* <?php echo $gender_error; ?></span>
 
-        <p>Communicatie voorkeur, via:</p>
-        <input type="radio" name="communicationpref" id="email" value="Email"><label for="email">Email</label><br>
-        <input type="radio" name="communicationpref" id="tel" value="Telefoon"><label for="tel">Telefoon</label><br>
-        <input type="radio" name="communicationpref" id="mail" value="Post"><label for="mail">Post</label><br>
+            <label for="fullname"><br>Voor- en achternaam: </label><input type="text" id="fullname" value="<?php echo $name; ?>" placeholder="Marie Jansen">
+            <span class="error">* <?php echo $name_error; ?></span>
 
-        <label for="message"><br>Uw bericht:<br></label><textarea id="message" rows="10" cols="60" placeholder="Typ hier uw bericht..."></textarea><br><br>
-        <input type="submit" value="Verzenden">
-    </form>
+            <label for="email"><br>Mailadres: </label><input type="email" id="email" value="<?php echo $email; ?>" placeholder="voorbeeld@mail.com">
+            <span class="error">* <?php echo $comm_error; ?></span>
 
+            <label for="phonenumber"><br>Telefoonnummer: </label><input type="tel" id="phonenumber" value="<?php echo $phone; ?>" placeholder="0612345678">
+            <span class="error">* <?php echo $comm_error; ?></span>
+
+            <label for="street"><br>Straat: </label><input type="text" id="street" value="<?php echo $street; ?>" placeholder="Voorbeeldstraat">
+            <label for="housingnumber"><br>Huisnummer: </label><input type="number" id="housingnumber" value="<?php echo $housenumber; ?>" placeholder="1">
+            <label for ="addition"><br>Toevoeging: </label><input type="text" id="addition" value="<?php echo $house_add; ?>" placeholder="A">
+            <label for="postalcode"><br>Postcode: </label><input type="text" id="postalcode" value="<?php echo $postalcode; ?>" placeholder="1234AB">
+            <label for="municipality"><br>Gemeente: </label><input type="text" id="municipality" value="<?php echo $municip; ?>" placeholder="Utrecht">
+
+            <p>Communicatie voorkeur, via:</p>
+            <input type="radio" name="communicationpref" id="email" value="Email"><label for="email">Email</label><br>
+            <input type="radio" name="communicationpref" id="tel" value="Telefoon"><label for="tel">Telefoon</label><br>
+            <input type="radio" name="communicationpref" id="mail" value="Post"><label for="mail">Post</label><br>
+            <span class="error">* <?php echo $comm_error; ?></span>
+
+            <label for="message"><br>Uw bericht:<br></label><textarea id="message" rows="10" cols="60" placeholder="Typ hier uw bericht..."></textarea><br><br>
+            <span class="error">* <?php echo $bericht_error; ?>></span>
+            <input type="submit" value="Verzenden">
+        </form>
+    <?php } else { ?>
+        <p>Bedankt, <?php echo $name; ?>, voor je reactie:</p>;
+        <div>Aanhef: <?php echo $gender; ?></div>
+        <div>Naam: <?php echo $name; ?></div>
+        <div>Tel: <?php echo $phone; ?></div>
+        <div>Email: <?php echo $email; ?></div>
+        <div>Adres: <?php echo $street; echo "," . $housenumber . $house_add;?></div>
+        <div>Woonplaats: <?php echo $postalcode . "," . $municip; ?></div>
+        <div>Communicatievoorkeur: <?php echo $comm; ?></div>
+
+    <?php } /* End of conditional showing */ ?>
     <footer>
         <p>&copy; Florian van der Steen 2024<br></p>
     </footer>
